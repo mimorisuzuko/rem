242067bd47a265ba27b0ec6153ff223f7faa945b
mimorisuzuko <hrky0206@gmail.com>
2017-03-12 15:38:28 +0900

    Removed unused

--- src/components/rem.jsx
+++ src/components/rem.jsx
@@ -4,19 +4,10 @@ const { Component } = React;
 class Rem extends Component {
 	render() {
 		const { props: { mode } } = this;
-		const { src } = Rem.MODEL[mode];
 
 		return (
 			<div className={mode} />
 		);
-	}
-
-	static get MODEL() {
-		return {
-			mjmj: {
-				src: '../../images/mjmj.png'
-			}
-		};
 	}
 }
 
